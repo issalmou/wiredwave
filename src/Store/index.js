c0efa80dc397ac74a3e0fa167f1d74bf6b0ded0b
issalmou <154550142+issalmou@users.noreply.github.com>
2025-08-31 23:33:09 +0100

    Delete src directory

--- src/Store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from '../PageCart/CartSlice';
-const Store = configureStore({
-    reducer : {
-        cart : cartReducer 
-    }
-});
-export default Store 
